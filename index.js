c221683a53d1d8206969b8d84fd7a7834ffd9d68
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-08-28 05:45:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config(); // Load environment variables from .env file
+const express = require('express');
+
 // index.js
 const sequelize = require('./db');
 const app = require('./app');
