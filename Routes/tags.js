de9b6797e3d54793cf7a36e81af8ec3e555a7013
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-09-06 21:29:51 -0500

    Update tags.js

--- Routes/tags.js
+++ Routes/tags.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const TagController = require('../controllers/TagController');
-
+//all routes start with /api/categories
 router.get('/', TagController.getAllTags);
-
+router.get('/:id', TagController.getSingleTags);
+router.post('/', TagController.createTags);
+router.put('/:id', TagController.updateTags);
 module.exports = router;
