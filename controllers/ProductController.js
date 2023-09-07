d15f14a58ff0da134baac2220d528d35b62e2bd2
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-09-06 21:28:48 -0500

    Update ProductController.js

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -10,4 +10,43 @@ module.exports = {
       res.status(500).json({ error: 'Internal server error' });
     }
   },
+  async createProducts (req, res) {
+    try {
+      const products = await Products.create(req.body);
+      res.json(products);
+    } catch (err) {
+      console.error('Error fetching products:', err);
+      res.status(500).json({ error: 'Internal server error' });
+    }  
+  },
+  async getSingleProducts(req, res) {
+    try {
+      const products = await Products.findByPk(req.params.id);
+      res.json(products);
+    } catch (err) {
+      console.error('Error fetching products:', err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
+  async updateProducts(req, res) {
+    try {
+      const productsId = req.params.id;
+      const updatedProducts = req.body;
+  
+      const products = await Products.findByPk(productsId);
+  
+      if (!products) {
+        return res.status(404).json({ error: 'Product not found' });
+      }
+  
+      await products.update(updatedProducts);
+  
+      res.json(products);
+    } catch (err) {
+      console.error('Error updating product:', err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
+  
 };
+
