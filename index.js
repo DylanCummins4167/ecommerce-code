4f4f0cd362e2aae91543c80e6b0d0084a2e799e7
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-09-06 21:35:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config(); // Load environment variables from .env file
-const express = require('express');
+const PORT = process.env.PORT || 3000;
 
 // index.js
 const sequelize = require('./db');
@@ -11,7 +11,7 @@ const app = require('./app');
     console.log('Database connection has been established successfully.');
     await sequelize.sync();
     console.log('Sequelize models synced to the database.');
-    app.listen(3000, () => console.log('Server started on http://localhost:3000'));
+    app.listen(PORT, () => console.log('Server started on http://localhost:3000'));
   } catch (err) {
     console.error('Unable to connect to the database:', err);
   }
