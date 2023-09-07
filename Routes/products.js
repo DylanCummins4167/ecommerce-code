9c78041aabcecd21ffd730b2e5c0d1b1fc288a4f
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-09-06 21:29:32 -0500

    Update products.js

--- Routes/products.js
+++ Routes/products.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const ProductController = require('../controllers/ProductController');
-
+//all routes start with /api/categories
 router.get('/', ProductController.getAllProducts);
-
+router.get('/:id', ProductController.getSingleProducts);
+router.post('/', ProductController.createProducts);
+router.put('/:id', ProductController.updateProducts);
 module.exports = router;
