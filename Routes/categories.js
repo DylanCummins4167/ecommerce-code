e8af32a047b28964df6fbd0a873b0bea66c6b7cf
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-09-06 21:29:22 -0500

    Update categories.js

--- Routes/categories.js
+++ Routes/categories.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const CategoryController = require('../controllers/CategoryController');
-
+//all routes start with /api/categories
 router.get('/', CategoryController.getAllCategories);
-
+router.get('/:id', CategoryController.getSingleCategory);
+router.post('/', CategoryController.createCategory);
+router.put('/:id', CategoryController.updateCategory);
 module.exports = router;
