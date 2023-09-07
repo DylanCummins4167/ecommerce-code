41531638c268e70a87e6d1c96fc50481ba93e9b2
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-09-06 21:28:32 -0500

    Update CategoryController.js

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -10,4 +10,42 @@ module.exports = {
       res.status(500).json({ error: 'Internal server error' });
     }
   },
+  async createCategory (req, res) {
+    try {
+      const categories = await Category.create(req.body);
+      res.json(categories);
+    } catch (err) {
+      console.error('Error fetching categories:', err);
+      res.status(500).json({ error: 'Internal server error' });
+    }  
+  },
+  async getSingleCategory(req, res) {
+    try {
+      const categories = await Category.findByPk(req.params.id);
+      res.json(categories);
+    } catch (err) {
+      console.error('Error fetching categories:', err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
+  async updateCategory(req, res) {
+    try {
+      const categoryId = req.params.id;
+      const updatedCategory = req.body;
+  
+      const category = await Category.findByPk(categoryId);
+  
+      if (!category) {
+        return res.status(404).json({ error: 'Category not found' });
+      }
+  
+      await category.update(updatedCategory);
+  
+      res.json(category);
+    } catch (err) {
+      console.error('Error updating category:', err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
+  
 };
